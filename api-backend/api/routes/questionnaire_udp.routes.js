742fe19a0c0f0c011d24e04b3ae1e5646c5ed7a3
ntua-el19206 <el19206@mail.ntua.gr>
2023-02-06 22:08:05 +0200

    nothing

--- api-backend/api/routes/questionnaire_udp.routes.js
+++ api-backend/api/routes/questionnaire_udp.routes.js
@@ -8,8 +8,9 @@ const router = express.Router();
 router.post('/', multer({ filename: (req, file, cb) => { cb(null, file.originalname) }, dest: "./uploads", }).single('file'), function (req, res) {
 
     let rawdata = fs.readFileSync('./uploads/'+req.file.filename);
-    let student = JSON.parse(rawdata);
-    console.log(student);
+    let sentdata = JSON.parse(rawdata);
+    console.log(sentdata);
+    
     res.status(200).json({
         status: "ok"
     })
