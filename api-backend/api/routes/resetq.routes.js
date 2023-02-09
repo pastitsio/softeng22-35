88c6c7ca226d4dd848c4bcf3616c902aec25cef3
ntua-el19206 <el19206@mail.ntua.gr>
2023-02-09 17:30:00 +0200

    resetq works

--- api-backend/api/routes/resetq.routes.js
+++ api-backend/api/routes/resetq.routes.js
@@ -4,20 +4,42 @@ const Session = require('../models/session.model')
 
 
 router.post("/:questionnaireId", async function (req, res, err) {
+    var i = 0
+    for await (const session of Session.find()) {
+        // console.log(session._id)
+        var seshas = session.questionnaires.filter(q => q.questionnaireId === req.params.questionnaireId)
+        if (seshas.length != 0) {
+            console.log(session._id)
 
-    Session.deleteOne({ questionnaireID: req.params.questionnaireId }).catch(err)
-    //     (err) => {
-    //     if (err) {
-    //         console.log(err)
-    //         res.status(500).json({
-    //             status: err
-    //         })
-    //     }
-    // }
+            Session.updateOne({ "_id": session._id }, { "$pull": { "questionnaires": { "questionnaireId": req.params.questionnaireId } } }, (err,session) => {
+                if (err) {
+                    res.status(500).json({
+                        status: "failed",
+                        message: err
+                    })
+                } else {
+                    res.status(200).json({
+                        status: "OK"
+                    })
+                }
+            })
+
+
+
+            // Session.updateOne({ _id: session._id }, { $pull: { questionnaires: { questionnaireId: req.params.questionnaireId } } })
+        }
+
+    }
+    res.status(200).json({})
+    // Session.deleteOne({ questionnaireID: req.params.questionnaireId }).catch(err)
+
 });
-// 200: success
-// "questionnaireID":"QQ000","session":"ATBP","answers":[{"qID":"P00","ans":"mail@mail.com"},{},{},{},{},{}]}
+
 
 
 
 module.exports = router;
+
+
+
+
