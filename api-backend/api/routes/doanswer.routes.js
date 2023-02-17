66f3f6aec02a959e1e69c8aad5fa1522d95c0d2b
ntua-el18406 <el18406@mail.ntua.gr>
2023-02-17 18:42:26 +0200

    frontent done.

--- api-backend/api/routes/doanswer.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const sessionController = require('../controllers/session.controller');
-
-router.post('/:questionnaireId/:questionId/:sessionId/:optionId', 
-            sessionController.postQuestionnaireQuestionSessionOption);
-
-module.exports = router;
