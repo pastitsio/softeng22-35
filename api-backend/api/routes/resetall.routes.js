583fb3132e80a90b1409f91bee98dc1fc12d4032
ntua-el18406 <el18406@mail.ntua.gr>
2023-02-19 08:47:51 +0200

    resetall deletes everything

--- api-backend/api/routes/resetall.routes.js
+++ api-backend/api/routes/resetall.routes.js
@@ -1,24 +1,32 @@
 const express = require("express");
-const { route } = require("./doanswer.routes");
 const router = express.Router();
 
 const Session = require('../models/session.model')
+const Questionnaire = require('../models/questionnaire.model')
+const Question = require('../models/question.model')
 
+router.post("/", async (req, res, next) => {
 
-router.post("/", (req, res, next) => {
+    try {
+        await Session.deleteMany({})
+    } catch (error){
+        next(error);
+    } 
+    
+    try {
+        await Questionnaire.deleteMany({})
+    } catch (error){
+        next(error);
+    } 
 
-    Session.deleteMany({}, (error) => {
-        if (error) {
-            res.status(200).json({
-                status: 'Failed',
-                message: error
-            })
-        }
-        else {
-            res.status(200).json({
-                status: 'OK'
-            })
-        }
+    try {
+        await Question.deleteMany({})
+    } catch (error){
+        next(error);    
+    } 
+
+    res.status(200).json({
+        "status" : "OK",
 
     })
 });// 200: success
